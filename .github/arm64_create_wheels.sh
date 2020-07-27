c3dde0f86a5a5bee07206b4e11b4491e9c7c1bea
Dish1306 <60138605+Dish1306@users.noreply.github.com>
2020-07-27 18:58:35 +0530

    Update arm64_create_wheels.sh

--- .github/arm64_create_wheels.sh
+++ .github/arm64_create_wheels.sh
@@ -4,6 +4,11 @@ $1 -m pip install --upgrade pip
 $1 -m pip install --upgrade setuptools wheel
 $1 setup.py bdist_wheel --plat-name=manylinux2014_aarch64 -d wheelhouse
 ls wheelhouse
-.travis/install.sh
+set -e
+set -x
+$1 -m install virtualenv
+$1 -m virtualenv ~/.venv
+source ~/.venv/bin/activate
+$1 -m pip install tox
 $1 -m pip install wheelhouse/bcrypt-3.1.8.dev1-cp$1-cp$1m-manylinux2014_aarch64.whl
 .travis/run.sh
