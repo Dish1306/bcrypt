e1041b1dced66d6a9c6466f8a3edc718dc5f838d
Dish1306 <60138605+Dish1306@users.noreply.github.com>
2020-07-30 17:19:49 +0530

    Update arm64_create_wheels.sh

--- .github/arm64_create_wheels.sh
+++ .github/arm64_create_wheels.sh
@@ -1,5 +1,4 @@
 yum update -y
-yum install -y coreutils curl
 /opt/_internal/cpython-$1*/bin/python -m pip install -U pip setuptools wheel
 /opt/_internal/cpython-$1*/bin/python setup.py bdist_wheel -d wheelhouse
 ls wheelhouse
