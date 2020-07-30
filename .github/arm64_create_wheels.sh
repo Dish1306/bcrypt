be3819a988d889b40c6d2b8ddd53619b2818a683
Dish1306 <60138605+Dish1306@users.noreply.github.com>
2020-07-30 19:15:39 +0530

    Update arm64_create_wheels.sh

--- .github/arm64_create_wheels.sh
+++ .github/arm64_create_wheels.sh
@@ -1,7 +1,7 @@
 yum update -y
 /opt/_internal/cpython-$1*/bin/python -m pip install -U pip setuptools wheel
 PY_LIMITED_API="--build-option --py-limited-api=cp3${BASH_REMATCH[1]}"
-/opt/_internal/cpython-$1*/bin/python pip wheel bcrypt --no-binary bcrypt --no-deps --wheel-dir=wheelhouse $PY_LIMITED_API
+/opt/_internal/cpython-$1*/bin/python -m pip wheel bcrypt --no-binary bcrypt --no-deps --wheel-dir=wheelhouse $PY_LIMITED_API
 #/opt/_internal/cpython-$1*/bin/python -m pip install -U pip setuptools wheel
 #/opt/_internal/cpython-$1*/bin/python setup.py bdist_wheel -d wheelhouse
 ls wheelhouse
