0c286a9cbd3123e06de539494eb10cc3aa87b341
Dish1306 <60138605+Dish1306@users.noreply.github.com>
2020-07-30 17:12:11 +0530

    Update arm64_create_wheels.sh

--- .github/arm64_create_wheels.sh
+++ .github/arm64_create_wheels.sh
@@ -1,15 +1,5 @@
 yum update -y
-yum install -y coreutils curl tree
-alias python=/opt/_internal/cpython-$1*/bin/python
-yum install -y python-pip
-python -m pip install -U setuptools wheel
-python setup.py bdist_wheel -d wheelhouse
+yum install -y coreutils curl
+/opt/_internal/cpython-$1*/bin/python -m pip install -U setuptools wheel
+/opt/_internal/cpython-$1*/bin/python setup.py bdist_wheel -d wheelhouse
 ls wheelhouse
-#set -e
-#set -x
-#$1 -m pip install virtualenv
-#$1 -m virtualenv ~/.venv
-#source ~/.venv/bin/activate
-#$1 -m pip install tox
-#$1 -m pip install wheelhouse/bcrypt-3.1.8.dev1-cp36-cp36m-manylinux2014_aarch64.whl
-#tox -e py36
