c5d0a707bc46301188f4ff9b3a29603aedf8f920
Dish1306 <60138605+Dish1306@users.noreply.github.com>
2020-07-31 11:15:47 +0530

    Update arm64_create_wheels.sh

--- .github/arm64_create_wheels.sh
+++ .github/arm64_create_wheels.sh
@@ -4,7 +4,10 @@ yum update -y
 #/opt/_internal/cpython-$1*/bin/pip wheel bcrypt --no-binary bcrypt --no-deps --wheel-dir=wheelhouse $PY_LIMITED_API
 #/opt/_internal/cpython-$1*/bin/python -m pip install -U pip setuptools wheel
 /opt/_internal/cpython-$1*/bin/python setup.py bdist_wheel --py-limited-api=cp36 -d wheelhouse
+echo "########################### python 3.6 ###########################################"
 /opt/_internal/cpython-3.6*/bin/python -m pip install wheelhouse/bcrypt-3.1.8.dev1-cp36-abi3-linux_aarch64.whl
+echo "########################### python 3.7 ###########################################"
 /opt/_internal/cpython-3.7*/bin/python -m pip install wheelhouse/bcrypt-3.1.8.dev1-cp36-abi3-linux_aarch64.whl
+echo "########################### python 3.8 ###########################################"
 /opt/_internal/cpython-3.8*/bin/python -m pip install wheelhouse/bcrypt-3.1.8.dev1-cp36-abi3-linux_aarch64.whl
 ls wheelhouse
