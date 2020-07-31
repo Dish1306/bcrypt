0b68034871464ea0635ad63c917b1310207cac66
Dish1306 <60138605+Dish1306@users.noreply.github.com>
2020-07-31 16:47:29 +0530

    Update arm64_create_wheels.sh

--- .github/arm64_create_wheels.sh
+++ .github/arm64_create_wheels.sh
@@ -1,8 +1,9 @@
 yum update -y
-/opt/_internal/cpython-$1*/bin/python -m virtualenv .venv
+/opt/python/${{ matrix.PYTHON }}/bin/python -m virtualenv .venv
 venv/bin/pip install setuptools wheel cffi six
+PY_LIMITED_API="--build-option --py-limited-api=cp3${BASH_REMATCH[1]}"
 #/opt/_internal/cpython-$1*/bin/python setup.py bdist_wheel --py-limited-api=cp36 -d wheelhouse
-.venv/bin/python setup.py bdist_wheel -d tmpwheelhouse --py-limited-api=cp36
+.venv/bin/pip wheel bcrypt --no-binary bcrypt --no-deps --wheel-dir=tmpwheelhouse $PY_LIMITED_API
 auditwheel repair tmpwheelhouse/bcrypt*.whl -w wheelhouse/
 .venv/bin/pip install bcrypt --no-index -f wheelhouse/
 .venv/bin/python -c "import bcrypt; password = b'super secret password';hashed = bcrypt.hashpw(password, bcrypt.gensalt());bcrypt.checkpw(password, hashed)"
