0c4eae4c623f640219e75f9f43f1847422ad400b
Dish1306 <60138605+Dish1306@users.noreply.github.com>
2020-07-31 13:49:42 +0530

    Update arm64_create_wheels.sh

--- .github/arm64_create_wheels.sh
+++ .github/arm64_create_wheels.sh
@@ -1,13 +1,12 @@
 yum update -y
 /opt/_internal/cpython-$1*/bin/python -m pip install -U pip setuptools wheel
-#PY_LIMITED_API="--py-limited-api=cp3${BASH_REMATCH[1]}"
-#/opt/_internal/cpython-$1*/bin/pip wheel bcrypt --no-binary bcrypt --no-deps --wheel-dir=wheelhouse $PY_LIMITED_API
-#/opt/_internal/cpython-$1*/bin/python -m pip install -U pip setuptools wheel
-/opt/_internal/cpython-$1*/bin/python setup.py bdist_wheel --py-limited-api=cp36 -d wheelhouse
+#/opt/_internal/cpython-$1*/bin/python setup.py bdist_wheel --py-limited-api=cp36 -d wheelhouse
+/opt/_internal/cpython-$1*/bin/python -m pip wheel bcrypt --no-binary bcrypt --no-deps --wheel-dir=tmpwheelhouse
+auditwheel repair tmpwheelhouse/bcrypt*.whl -w wheelhouse/
+/opt/_internal/cpython-$1*/bin/python -m pip install bcrypt --no-index -f wheelhouse/
+/opt/_internal/cpython-$1*/bin/python -c "import bcrypt; password = b'super secret password';hashed = bcrypt.hashpw(password, bcrypt.gensalt());bcrypt.checkpw(password, hashed)"
+mkdir bcrypt-wheelhouse
+mv wheelhouse/bcrypt*.whl bcrypt-wheelhouse/
 echo "########################### python 3.6 ###########################################"
 /opt/_internal/cpython-3.6*/bin/python -m pip install wheelhouse/bcrypt-3.1.8.dev1-cp36-abi3-linux_aarch64.whl
-echo "########################### python 3.7 ###########################################"
-/opt/_internal/cpython-3.7*/bin/python -m pip install wheelhouse/bcrypt-3.1.8.dev1-cp36-abi3-linux_aarch64.whl
-echo "########################### python 3.8 ###########################################"
-/opt/_internal/cpython-3.8*/bin/python -m pip install wheelhouse/bcrypt-3.1.8.dev1-cp36-abi3-linux_aarch64.whl
 ls wheelhouse
