a8ca617f2e212e77510b7d4a80f682014493d7a7
Dish1306 <60138605+Dish1306@users.noreply.github.com>
2020-07-31 17:57:36 +0530

    Update arm64_create_wheels.sh

--- .github/arm64_create_wheels.sh
+++ .github/arm64_create_wheels.sh
@@ -2,7 +2,7 @@ yum update -y
 /opt/python/$1/bin/pip install virtualenv
 /opt/python/$1/bin/python -m virtualenv .venv
 .venv/bin/pip install setuptools wheel cffi six
-PY_LIMITED_API="--build-option --py-limited-api=cp3${BASH_REMATCH[1]}"
+PY_LIMITED_API="--py-limited-api=cp3${BASH_REMATCH[1]}"
 #/opt/_internal/cpython-$1*/bin/python setup.py bdist_wheel --py-limited-api=cp36 -d wheelhouse
 .venv/bin/pip wheel bcrypt --no-binary bcrypt --no-deps --wheel-dir=tmpwheelhouse $PY_LIMITED_API
 auditwheel repair tmpwheelhouse/bcrypt*.whl -w wheelhouse/
