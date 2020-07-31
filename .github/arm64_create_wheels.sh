d8d957da7e552bfed872bd303ad7ed0cf37cd4de
Dish1306 <60138605+Dish1306@users.noreply.github.com>
2020-07-31 20:03:39 +0530

    Update arm64_create_wheels.sh

--- .github/arm64_create_wheels.sh
+++ .github/arm64_create_wheels.sh
@@ -3,9 +3,9 @@ yum update -y
 /opt/python/$1/bin/python -m virtualenv .venv
 .venv/bin/pip install setuptools wheel cffi six
 REGEX="cp3([0-9])*"
-          if [[ "$1" =~ $REGEX ]]; then
-              PY_LIMITED_API="--py-limited-api=cp3${BASH_REMATCH[1]}"
-          fi
+if [[ "$1" =~ $REGEX ]]; then
+    PY_LIMITED_API="--py-limited-api=cp3${BASH_REMATCH[1]}"
+fi
 #.venv/bin/pip wheel bcrypt --no-binary bcrypt --no-deps --wheel-dir=tmpwheelhouse $PY_LIMITED_API
 .venv/bin/python setup.py bdist_wheel $PY_LIMITED_API -d tmpwheelhouse
 auditwheel repair tmpwheelhouse/bcrypt*.whl -w wheelhouse/
