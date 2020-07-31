bf35f0ce8bf35015bc0f9bee7e250d1dd95f7425
Dish1306 <60138605+Dish1306@users.noreply.github.com>
2020-07-31 16:24:53 +0530

    Update arm64_create_wheels.sh

--- .github/arm64_create_wheels.sh
+++ .github/arm64_create_wheels.sh
@@ -1,12 +1,8 @@
 yum update -y
-/opt/_internal/cpython-$1*/bin/python -m pip install -U pip setuptools wheel cffi six
+/opt/_internal/cpython-$1*/bin/python -m virtualenv .venv
+venv/bin/pip install setuptools wheel cffi six
 #/opt/_internal/cpython-$1*/bin/python setup.py bdist_wheel --py-limited-api=cp36 -d wheelhouse
-/opt/_internal/cpython-$1*/bin/python setup.py bdist_wheel -d tmpwheelhouse --py-limited-api=cp36
+.venv/bin/python setup.py bdist_wheel -d tmpwheelhouse --py-limited-api=cp36
 auditwheel repair tmpwheelhouse/bcrypt*.whl -w wheelhouse/
-/opt/_internal/cpython-$1*/bin/python -m pip install bcrypt --no-index -f wheelhouse/
-/opt/_internal/cpython-$1*/bin/python -c "import bcrypt; password = b'super secret password';hashed = bcrypt.hashpw(password, bcrypt.gensalt());bcrypt.checkpw(password, hashed)"
-mkdir bcrypt-wheelhouse
-mv wheelhouse/bcrypt*.whl bcrypt-wheelhouse/
-echo "########################### python 3.6 ###########################################"
-/opt/_internal/cpython-3.6*/bin/python -m pip install wheelhouse/bcrypt-3.1.8.dev1-cp36-abi3-linux_aarch64.whl
-ls wheelhouse
+.venv/bin/pip install bcrypt --no-index -f wheelhouse/
+.venv/bin/python -c "import bcrypt; password = b'super secret password';hashed = bcrypt.hashpw(password, bcrypt.gensalt());bcrypt.checkpw(password, hashed)"
