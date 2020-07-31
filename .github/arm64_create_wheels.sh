1bc193e943506be8e25446067fadf4a71beec2d3
Dish1306 <60138605+Dish1306@users.noreply.github.com>
2020-07-31 17:44:19 +0530

    Update arm64_create_wheels.sh

--- .github/arm64_create_wheels.sh
+++ .github/arm64_create_wheels.sh
@@ -1,14 +1,15 @@
 yum update -y
-#/opt/python/$1/bin/python -m virtualenv .venv
-#venv/bin/pip install setuptools wheel cffi six
+/opt/python/$1/bin/pip install virtualenv
+/opt/python/$1/bin/python -m virtualenv .venv
+.venv/bin/pip install setuptools wheel cffi six
 PY_LIMITED_API="--build-option --py-limited-api=cp3${BASH_REMATCH[1]}"
 #/opt/_internal/cpython-$1*/bin/python setup.py bdist_wheel --py-limited-api=cp36 -d wheelhouse
-#.venv/bin/pip wheel bcrypt --no-binary bcrypt --no-deps --wheel-dir=tmpwheelhouse $PY_LIMITED_API
-#auditwheel repair tmpwheelhouse/bcrypt*.whl -w wheelhouse/
-#.venv/bin/pip install bcrypt --no-index -f wheelhouse/
-#.venv/bin/python -c "import bcrypt; password = b'super secret password';hashed = bcrypt.hashpw(password, bcrypt.gensalt());bcrypt.checkpw(password, hashed)"
-/opt/python/$1/bin/pip install setuptools wheel cffi six
-/opt/python/$1/bin/pip wheel bcrypt --no-binary bcrypt --no-deps --wheel-dir=tmpwheelhouse $PY_LIMITED_API
+.venv/bin/pip wheel bcrypt --no-binary bcrypt --no-deps --wheel-dir=tmpwheelhouse $PY_LIMITED_API
 auditwheel repair tmpwheelhouse/bcrypt*.whl -w wheelhouse/
-/opt/python/$1/bin/pip install bcrypt --no-index -f wheelhouse/
-/opt/python/$1/bin/pip/python -c "import bcrypt; password = b'super secret password';hashed = bcrypt.hashpw(password, bcrypt.gensalt());bcrypt.checkpw(password, hashed)
+.venv/bin/pip install bcrypt --no-index -f wheelhouse/
+.venv/bin/python -c "import bcrypt; password = b'super secret password';hashed = bcrypt.hashpw(password, bcrypt.gensalt());bcrypt.checkpw(password, hashed)"
+#/opt/python/$1/bin/pip install setuptools wheel cffi six
+#/opt/python/$1/bin/pip wheel bcrypt --no-binary bcrypt --no-deps --wheel-dir=tmpwheelhouse $PY_LIMITED_API
+#auditwheel repair tmpwheelhouse/bcrypt*.whl -w wheelhouse/
+#/opt/python/$1/bin/pip install bcrypt --no-index -f wheelhouse/
+#/opt/python/$1/bin/pip/python -c "import bcrypt; password = b'super secret password';hashed = bcrypt.hashpw(password, bcrypt.gensalt());bcrypt.checkpw(password, hashed)
